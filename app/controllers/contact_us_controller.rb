3b8b4f0ca7e3600cdc3693538a92ff8e6d0e777d
EZ <testrailsemail@gmail.com>
2022-12-15 14:38:32 +0200

    Turbo frames and Turbo streams example

--- app/controllers/contact_us_controller.rb
+++ app/controllers/contact_us_controller.rb
@@ -2,5 +2,32 @@
 
 class ContactUsController < ApplicationController
   def new
+    @message = Message.new
+  end
+
+  def index; end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      respond_to do |format|
+        format.turbo_stream do
+          render(turbo_stream: turbo_stream.prepend(
+            :messages,
+            partial: "contact_us/message",
+            locals: { message: @message }
+          ))
+        end
+      end
+    else
+      flash[:error] = "Cannot save message"
+      redirect_to welcome_path
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @messages = Message.order(created_at: :desc)
   end
 end
